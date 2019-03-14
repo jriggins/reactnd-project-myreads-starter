79a06858f77c84254d6ddc40f429e2a824ad6dea
Jesse Riggins <jriggins@users.noreply.github.com>
2019-03-14 07:51:19 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -8,29 +8,55 @@ import Search from './Search.js'
 class BooksApp extends React.Component {
   state = {
     books: [],
+    booksFromSearch: [],
+    myBooks: [],
+    query: '',
   }
 
   componentDidMount() {
-    this.getAllBooksFromServer()
+    this.getMyBooks()
       .then(this.setBooksStateFromServer)
   }
 
-  getAllBooksFromServer = () => {
+  getMyBooks = () => {
     return BooksApi.getAll()
       .then(this.fromJsonToBook)
   }
 
-  fromJsonToBook = (books) => books.map(this.getCoverUrlFrom)
+  getAllBooksFromSearch = (query) => {
+    return BooksApi.search(query)
+      .then(this.fromJsonToBook)
+      .then((books) => books.map((book) => this.state.myBooks[book.id] || book))
+  }
+
+  onSearchRequested = (query) => {
+    this.setState({query: query})
+    this.getAllBooksFromSearch(query)
+      .then(this.updateBookFromSearch)
+  }
+
+  updateBookFromSearch = (booksFromSearch) => {
+    this.setState({booksFromSearch})
+  }
+
+  fromJsonToBook = (books) => {
+    books = (books && !books.error) ? books : []
+    return books.filter((book) => book.imageLinks !== undefined).map(this.getCoverUrlFrom)
+  }
 
   getCoverUrlFrom = (book) => ({...book, coverUrl: book.imageLinks.thumbnail})
 
-  setBooksStateFromServer = (books) => this.setState({books})
+  setBooksStateFromServer = (books) => this.setState({books, myBooks: this.getMyBooksFromList(books)})
 
   updateBookState = (updatedBook) => {
     this.setState((prevState) => ({
-      books: [...prevState.books.filter((book) => book.id !== updatedBook.id), updatedBook]
+      books: [...prevState.books.filter((book) => book.id !== updatedBook.id), updatedBook],
+      myBooks: {...prevState.myBooks, [updatedBook.id]: updatedBook},
+      booksFromSearch: prevState.booksFromSearch.map((book) => prevState.myBooks[book.id] || book)
     }))
   }
+
+  getMyBooksFromList = (books) => { return books.reduce((obj, book) => (Object.assign(obj, {[book.id]: book})), {}) }
 
   updateBookshelf = (updatedBook) => {
     return BooksApi
@@ -48,7 +74,10 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route exact path="/" render={() => <Home books={this.state.books} onBookshelfChange={this.onBookshelfChange}/>}/>
-        <Route exact path="/search" render={() => <Search/>}/>
+        <Route exact path="/search" render={() => <Search books={this.state.booksFromSearch}
+                                                          query={this.state.query}
+                                                          onSearchRequested={this.onSearchRequested}
+                                                          onBookshelfChange={this.onBookshelfChange}/>}/>
       </div>
     )
   }
